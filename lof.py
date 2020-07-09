035f8f171004ff401a30d35da89ad4aa3507832e
caotc <250622148@qq.com>
2020-07-09 14:55:49 +0800

    增加监控基金
    增加监控基金

--- lof.py
+++ lof.py
@@ -11,14 +11,14 @@ class LOF:
         self.s = {
             "代码": "fund_id",
             "名称": "fund_nm",
-            # "现价": "price",
+            "现价": "price",
             "涨幅": "increase_rt",
-            # "基金净值": "fund_nav",
-            # "实时估值": "estimate_value",
+            "基金净值": "fund_nav",
+            "实时估值": "estimate_value",
             "溢价率": "discount_rt",
         }
         # 可在列表中添加想要监控的LOF
-        self.LOFList = [161005, 163402, 163417]
+        self.LOFList = [161005, 163402, 163417, 165309]
         self.LOFList.sort()
 
         self.session = requests.Session()
